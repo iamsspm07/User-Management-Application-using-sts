59b7e66946051796f47859da5b1258eea3abd697
SUJIT SHIBAPRASAD MAITY <sujitmaity.rs@gmail.com>
2022-09-11 21:50:56 +0530

    User Management Application

---
+++ src/main/java/com/sujitmaity/usermanagementapp/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.sujitmaity.usermanagementapp.dao;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.sujitmaity.usermanagementapp.userentity.User;
+
+public interface UserDao extends JpaRepository<User,Integer> {
+
+	public void deleteByUserId(Integer user1);
+
+	public List<User> findByFirstNameAndUserCompany(String user, String user1);
+
+}
